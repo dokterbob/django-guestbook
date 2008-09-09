f05918a4011e8c827d7f62f8ce5d5feb05d2ebc9
Mathijs de Bruin <mathijs@visualspace.nl>
2008-09-09 20:37:00 +0200

    Started port of guestbook app to Django trunk - pagination is left to do.

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
-from models import *
+from views import *
 
-urlpatterns = patterns('amfibi.guestbook.views',
-     (r'^$', 'last_page'),
-     (r'^add/', 'add_entry'),
-     (r'^(?P<page>[0-9]+)/$', 'guestbook_list'),
+urlpatterns = patterns('',
+     (r'^$', last_page),
+     (r'^add/', add_entry),
+     (r'^(?P<page>[0-9]+)/$', guestbook_list),
 )
 
